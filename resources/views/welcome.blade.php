09ff1aefc9f54da4ba3b84f10bf52aea1a2d9850
jcoloj <jcoloj@jupi.tech>
2021-04-23 23:59:05 -0600

    d

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
 
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                {{-- <li><a href="#" class="nav-link px-2 text-white">Tu queja</a></li> --}}
-               <li><a href="#" class="nav-link px-2 text-white">Queja</a></li>
+               <li><a href="#" class="nav-link px-2 text-white">DIACO</a></li>
             </ul>
 
             <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" ng-submit="buscarQueja()">
